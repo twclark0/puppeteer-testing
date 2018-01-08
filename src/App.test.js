61f3be09edd2dd2d0fc67e977dd8fc1bc7a9d07f
twclark0 <tylerc644@gmail.com>
2018-01-08 12:53:48 -0700

    Update test and package json

--- src/App.test.js
+++ src/App.test.js
@@ -4,10 +4,7 @@ const isDebugging = () => {
   let debugging_mode = {
     headless: false,
     slowMo: 250,
-    // executablePath: string (path to chrome or custom chromium)
-    // timeout: number (default is 30 seconds, 0 means disable)
-    // ignoreHTTPSErrors: bool (Whether to ignore HTTPS errors during navigation. Defaults to false.)
-    devtools: true  // headless option will be set to false
+    devtools: true  
   }
   return process.env.NODE_ENV === 'debug' ? debugging_mode : false;
 };
@@ -20,15 +17,9 @@ test('First test', async () => {
     viewport: {
       width: 500,
       height: 2400
-      // isMobile: bool,
-      // hasTouch: bool, 
-      // isLandscape: bool
     },
      userAgent: ''
-  }, 16000)
-
-  // page.setUserAgent(userAgent)
-  // page.setViewport(viewport)
+  })
 
    await page.goto('http://localhost:3000/')
    await page.waitForSelector('.App-title')
@@ -39,12 +30,4 @@ test('First test', async () => {
 
    browser.close()
 
-})
-
-
-// Puppeteer requires at least Node v6.4.0, but the examples below use async/await which is only supported in Node v7.6.0 or greater
-
-// The easiest way to get started with headless mode is to open the Chrome binary from the command line. If you've got Chrome 59+ installed, start Chrome with the --headless flag
-
-// The --repl flag runs Headless in a mode where you can evaluate JS expressions in the browser, right from the command line:
-
+}, 16000)
